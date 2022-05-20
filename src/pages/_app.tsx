ee95ea0b1c2aca619c6c32225dc3c6ba173417f2
Your Name <mamsheikh123@gmail.com>
2022-05-20 17:01:47 +0100

    apollo client setup for SSR & CSR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
+import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { theme } from '../chakra/theme';
+import { useApollo } from '../lib/apolloClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />;
-    </ChakraProvider>
+    <ApolloProvider client={apolloClient}>
+      <Head>
+        <title>Reddit</title>
+        <meta name='description' content='A Reddit clone' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />;
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,36 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import { gql, useQuery } from '@apollo/client';
+import type {
+  NextPage,
+  GetServerSideProps,
+  GetServerSidePropsContext,
+} from 'next';
 
-const Home: NextPage = () => {
-  return <div>hello world</div>;
+import { addApolloState, initializeApollo } from '../lib/apolloClient';
+import { prisma } from '../lib/prisma';
+
+const testQuery = gql`
+  {
+    test(bool: false)
+  }
+`;
+
+const Home = () => {
+  const { data } = useQuery(testQuery, {
+    notifyOnNetworkStatusChange: true,
+  });
+  return <div>{JSON.stringify(data?.test)}</div>;
+};
+
+export const getServerSideProps = async ({
+  req,
+}: GetServerSidePropsContext) => {
+  const apolloClient = initializeApollo({ ctx: { req, prisma } });
+
+  await apolloClient.query({ query: testQuery });
+
+  return addApolloState(apolloClient, {
+    props: {},
+  });
 };
 
 export default Home;
