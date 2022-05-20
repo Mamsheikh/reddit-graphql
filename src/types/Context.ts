3365dabe2ddef24e981cbdca353120c1e890b78f
Your Name <mamsheikh123@gmail.com>
2022-05-20 15:12:09 +0100

    prisma setup

--- src/types/Context.ts
+++ src/types/Context.ts
@@ -1,12 +1,13 @@
-import { IncomingMessage } from "http";
-import { NextApiRequest } from "next";
-import { NextApiRequestCookies } from "next/dist/server/api-utils";
-
+import { PrismaClient } from '@prisma/client';
+import { IncomingMessage } from 'http';
+import { NextApiRequest } from 'next';
+import { NextApiRequestCookies } from 'next/dist/server/api-utils';
 
 export interface Context {
-    req:
+  req:
     | NextApiRequest
     | (IncomingMessage & {
         cookies: NextApiRequestCookies;
       });
+  prisma: PrismaClient;
 }
